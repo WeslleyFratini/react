c1ab9edd44b58ba78f7e612737dfdd6bd62a577c
WeslleyFratini <weeslleey354@gmail.com>
2021-06-03 19:04:25 -0300

    criou o component de task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Tasks from "./components/task"
+import './App.css'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar programação',
+      completed: false
+    },
+    {
+      id: '2',
+      title: 'Estudar matematica',
+      completed: true,
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="container">
+        <Tasks />
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
